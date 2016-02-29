5369a8f96f171a8e2ecc6856f9ab95f8eada3037
Thunder Raven-Stoker <thunder@vanqard.com>
2016-02-29 16:46:27 +0000

    Added plugin to copy build reports to web root

---
+++ PHPCI/Plugin/DeployStaticAnalysis.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PHPCI - Continuous Integration for PHP
+ *
+ * @copyright    Copyright 2014, Block 8 Limited.
+ * @license      https://github.com/Block8/PHPCI/blob/master/LICENSE.md
+ * @link         https://www.phptesting.org/
+ */
+
+namespace PHPCI\Plugin;
+
+use PHPCI\Builder;
+use PHPCI\Model\Build;
+use PHPCI\Helper\Lang;
+
+/**
+ * Copy Build Plugin - Copies the entire build to another directory.
+ * @author       Dan Cryer <dan@block8.co.uk>
+ * @package      PHPCI
+ * @subpackage   Plugins
+ */
+class DeployStaticAnalysis implements \PHPCI\Plugin
+{
+    protected $phpci;
+    protected $build;
+    protected $options;
+
+    /**
+     * Set up the plugin, configure options, etc.
+     * @param Builder $phpci
+     * @param Build $build
+     * @param array $options
+     */
+    public function __construct(Builder $phpci, Build $build, array $options = array())
+    {
+        $this->phpci     = $phpci;
+        $this->build     = $build;
+
+        $this->options = $options;
+    }
+
+
+    /**
+     * Moves the static analysis log files into doc root before the build is destroyed
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function execute()
+    {
+        $srcDir = $this->build->getBuildPath() . '/build/logs/report';
+        $destDir = APPLICATION_PATH . 'public/reports/' . $this->build->getId();
+
+        // Attempt to move the directory
+        return rename($srcDir, $destDir);
+    }
+}
